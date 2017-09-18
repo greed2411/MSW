38e712ff0bac696c199647896f25f07cda1eab7e
Jaivarsan <jaiimmortal@gmail.com>
2017-09-19 01:15:27 +0530

    home, about, contact, profile more jinja templating

--- app/__init__.py
+++ app/__init__.py
@@ -4,26 +4,156 @@ app = Flask(__name__)
 app.secret_key = 'supposed to be a secret'
 
 
+
+@app.route('/')
+def home_page():
+
+    return render_template('home.html')
+
 @app.route('/about/')
 def about():
     title = "About Us"
-    content = "Team work mattters."
-    return render_template('wrapper.html', title = title, content = content)
+    content = "Team work matters."
+    return render_template('about.html', title = title, content = content)
 
 
 @app.route('/contact/')
 def contact():
     title = "Contact Us"
     content = "Feel free to drop by and say hi."
-    return render_template('wrapper.html', title = title, content = content)
+    return render_template('contact.html', title = title, content = content)
 
 
-@app.route('/')
-def home_page():
+# @app.route('/about/shreyas')
+# @app.route('/about/axel')
+# @app.route('/about/joshua')
+# @app.route('/about/shaishav')
+# @app.route('/about/aakriti')
+# @app.route('/about/jaivarsan')
 
-    content = "Team work matters."
-    return render_template('home.html', content = content)
 
+@app.route('/about/<name>')
+def profile(name):
+    if name in ['shreyas', 'axel', 'joshua', 'shaishav', 'aakriti', 'jaivarsan']:
+        
+        if name == 'shreyas':
+            projects_link = ''
+            skills_link = ''
+            cv_name = ''
+            facebook_link = ''
+            twitter_link = ''
+            instagram_link = ''
+            github_link = ''
+            linkedin_link = ''
+            return render_template('profile.html', 
+                                                name = name, 
+                                                projects_link = projects_link, 
+                                                skills_link = skills_link, 
+                                                cv_name = cv_name, 
+                                                facebook_link = facebook_link, 
+                                                twitter_link = twitter_link,
+                                                instagram_link = instagram_link, 
+                                                github_link = github_link, 
+                                                linkedin_link = linkedin_link)
+        
+        elif name == 'axel':
+            projects_link = ''
+            skills_link = ''
+            cv_name = ''
+            facebook_link = ''
+            twitter_link = ''
+            instagram_link = ''
+            github_link = ''
+            linkedin_link = ''
+            return render_template('profile.html', 
+                                                name = name, 
+                                                projects_link = projects_link, 
+                                                skills_link = skills_link, 
+                                                cv_name = cv_name, 
+                                                facebook_link = facebook_link, 
+                                                twitter_link = twitter_link,
+                                                instagram_link = instagram_link, 
+                                                github_link = github_link, 
+                                                linkedin_link = linkedin_link)
+
+        elif name == 'joshua':
+            projects_link = ''
+            skills_link = ''
+            cv_name = ''
+            facebook_link = ''
+            twitter_link = ''
+            instagram_link = ''
+            github_link = ''
+            linkedin_link = ''
+            return render_template('profile.html', 
+                                                name = name, 
+                                                projects_link = projects_link, 
+                                                skills_link = skills_link, 
+                                                cv_name = cv_name, 
+                                                facebook_link = facebook_link, 
+                                                twitter_link = twitter_link,
+                                                instagram_link = instagram_link, 
+                                                github_link = github_link, 
+                                                linkedin_link = linkedin_link)
+        elif name == 'shaishav':
+            projects_link = ''
+            skills_link = ''
+            cv_name = ''
+            facebook_link = ''
+            twitter_link = ''
+            instagram_link = ''
+            github_link = ''
+            linkedin_link = ''
+            return render_template('profile.html', 
+                                                name = name, 
+                                                projects_link = projects_link, 
+                                                skills_link = skills_link, 
+                                                cv_name = cv_name, 
+                                                facebook_link = facebook_link, 
+                                                twitter_link = twitter_link,
+                                                instagram_link = instagram_link, 
+                                                github_link = github_link, 
+                                                linkedin_link = linkedin_link)
+
+        elif name == 'aakriti':
+            projects_link = ''
+            skills_link = ''
+            cv_name = ''
+            facebook_link = ''
+            twitter_link = ''
+            instagram_link = ''
+            github_link = ''
+            linkedin_link = ''
+            return render_template('profile.html', 
+                                                name = name, 
+                                                projects_link = projects_link, 
+                                                skills_link = skills_link, 
+                                                cv_name = cv_name, 
+                                                facebook_link = facebook_link, 
+                                                twitter_link = twitter_link,
+                                                instagram_link = instagram_link, 
+                                                github_link = github_link, 
+                                                linkedin_link = linkedin_link)
+                                                
+        elif name == 'jaivarsan':
+            projects_link = 'https://github.com/greed2411?tab=repositories'
+            skills_link = ''
+            cv_name = ''
+            facebook_link = 'https://www.facebook.com/jaivarsan.bala'
+            twitter_link = 'https://twitter.com/greed2411'
+            instagram_link = ''
+            github_link = 'https://github.com/greed2411'
+            linkedin_link = ''
+            return render_template('profile.html', 
+                                                name = name, 
+                                                projects_link = projects_link, 
+                                                skills_link = skills_link, 
+                                                cv_name = cv_name, 
+                                                facebook_link = facebook_link, 
+                                                twitter_link = twitter_link,
+                                                instagram_link = instagram_link, 
+                                                github_link = github_link, 
+                                                linkedin_link = linkedin_link)
 
 @app.errorhandler(404)
 def page_not_found(error):
@@ -32,7 +162,7 @@ def page_not_found(error):
     """
     title = "404 bud."
     content = "what you are searching for isn't available."
-    return render_template('wrapper.html', title = title, content = content), 404
+    return render_template('error.html', title = title, content = content), 404
 
 
 @app.errorhandler(400)
@@ -40,7 +170,7 @@ def bad_request(error):
 
     title = "Aaaah ..."
     content = "Bad request, the server failed to help you."
-    return render_template('wrapper.html' , title = title, content = content), 400
+    return render_template('error.html' , title = title, content = content), 400
 
 
 if __name__ == '__main__':
