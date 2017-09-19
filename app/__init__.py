c0a90527aec56bd290f3bc1e3feeb8d85f8a2d4f
Jaivarsan <jaiimmortal@gmail.com>
2017-09-19 22:39:51 +0530

    abort(), typeit, aos, cdn

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, abort
 
 app = Flask(__name__)
 app.secret_key = 'supposed to be a secret'
@@ -27,35 +27,9 @@ def contact():
 @app.route('/about/<name>')
 def profile(name):
 
-    # list_of_members = ['shreyas', 'axel', 'joshua', 'shaishav', 'aakriti', 'jaivarsan']
-    
-    # if name in list_of_members:
-    #     position = list_of_members.index(name)
-
-    # member_details{
-    #     0 : [
-
-    #     ],
-    #     1 : [
-
-    #     ],
-    #     2 : [
-
-    #     ],
-    #     3 : [
-
-    #     ],
-    #     4 : [
-
-    #     ],
-    #     5 : [
-
-    #     ]
-    # }
     if name in ['shreyas', 'axel', 'joshua', 'shaishav', 'aakriti', 'jaivarsan']:
         if name == 'shreyas':
-            projects_link = ''
-            skills_link = ''
+            projects_link = ''            
             cv_name = ''
             facebook_link = ''
             twitter_link = ''
@@ -65,8 +39,7 @@ def profile(name):
             mail_link = 'mailto:'
             return render_template('profile.html', 
                                                 name = name, 
-                                                projects_link = projects_link, 
-                                                skills_link = skills_link, 
+                                                projects_link = projects_link,                                                  
                                                 cv_name = cv_name, 
                                                 facebook_link = facebook_link, 
                                                 twitter_link = twitter_link,
@@ -76,8 +49,7 @@ def profile(name):
                                                 mail_link = mail_link)
         
         elif name == 'axel':
-            projects_link = ''
-            skills_link = ''
+            projects_link = ''            
             cv_name = ''
             facebook_link = ''
             twitter_link = ''
@@ -87,8 +59,7 @@ def profile(name):
             mail_link = 'mailto:'
             return render_template('profile.html', 
                                                 name = name, 
-                                                projects_link = projects_link, 
-                                                skills_link = skills_link, 
+                                                projects_link = projects_link,                                                  
                                                 cv_name = cv_name, 
                                                 facebook_link = facebook_link, 
                                                 twitter_link = twitter_link,
@@ -98,8 +69,7 @@ def profile(name):
                                                 mail_link = mail_link)
 
         elif name == 'joshua':
-            projects_link = ''
-            skills_link = ''
+            projects_link = ''            
             cv_name = ''
             facebook_link = ''
             twitter_link = ''
@@ -109,8 +79,7 @@ def profile(name):
             mail_link = 'mailto:'
             return render_template('profile.html', 
                                                 name = name, 
-                                                projects_link = projects_link, 
-                                                skills_link = skills_link, 
+                                                projects_link = projects_link,                                                  
                                                 cv_name = cv_name, 
                                                 facebook_link = facebook_link, 
                                                 twitter_link = twitter_link,
@@ -119,8 +88,7 @@ def profile(name):
                                                 linkedin_link = linkedin_link,
                                                 mail_link = mail_link)
         elif name == 'shaishav':
-            projects_link = ''
-            skills_link = ''
+            projects_link = ''            
             cv_name = ''
             facebook_link = ''
             twitter_link = ''
@@ -130,8 +98,7 @@ def profile(name):
             mail_link = 'mailto:'
             return render_template('profile.html', 
                                                 name = name, 
-                                                projects_link = projects_link, 
-                                                skills_link = skills_link, 
+                                                projects_link = projects_link,                                                  
                                                 cv_name = cv_name, 
                                                 facebook_link = facebook_link, 
                                                 twitter_link = twitter_link,
@@ -141,8 +108,7 @@ def profile(name):
                                                 mail_link = mail_link)
 
         elif name == 'aakriti':
-            projects_link = ''
-            skills_link = ''
+            projects_link = ''            
             cv_name = ''
             facebook_link = ''
             twitter_link = ''
@@ -152,8 +118,7 @@ def profile(name):
             mail_link = 'mailto:'
             return render_template('profile.html', 
                                                 name = name, 
-                                                projects_link = projects_link, 
-                                                skills_link = skills_link, 
+                                                projects_link = projects_link,                                                  
                                                 cv_name = cv_name, 
                                                 facebook_link = facebook_link, 
                                                 twitter_link = twitter_link,
@@ -163,8 +128,7 @@ def profile(name):
                                                 mail_link = mail_link)
                                                 
         elif name == 'jaivarsan':
-            projects_link = 'https://github.com/greed2411?tab=repositories'
-            skills_link = ''
+            projects_link = 'https://github.com/greed2411?tab=repositories'            
             cv_name = 'jaivarsan'
             facebook_link = 'https://www.facebook.com/jaivarsan.bala'
             twitter_link = 'https://twitter.com/greed2411'
@@ -174,8 +138,7 @@ def profile(name):
             mail_link = 'mailto:jaiimmortal@gmail.com'
             return render_template('profile.html', 
                                                 name = name, 
-                                                projects_link = projects_link, 
-                                                skills_link = skills_link, 
+                                                projects_link = projects_link,                                                  
                                                 cv_name = cv_name, 
                                                 facebook_link = facebook_link, 
                                                 twitter_link = twitter_link,
@@ -183,6 +146,10 @@ def profile(name):
                                                 github_link = github_link, 
                                                 linkedin_link = linkedin_link,
                                                 mail_link = mail_link)
+    
+    else:
+
+        abort(404)
 
 @app.errorhandler(404)
 def page_not_found(error):
--- app/static/js/init.js
+++ app/static/js/init.js
@@ -1,9 +1,11 @@
-(function($){
-  $(function(){
+(function ($) {
+  $(function () {
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+    $('.scrollspy').scrollSpy();
+
+
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
-
