420ceb12c3ee88d384b31b96e4b718d6bb44f65a
Yaswant Narayan <yoyaswant@gmail.com>
2017-09-19 15:52:11 +0530

    Added Typeit.JS

--- js/init.js
+++ js/init.js
@@ -1,9 +1,21 @@
-(function($){
-  $(function(){
+AOS.init();
+
+(function ($) {
+  $(function () {
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
     $('.scrollspy').scrollSpy();
+
+    $('#introtext').typeIt({
+      strings: ["Mechatronics.", "Machine Learning."],
+      speed: 50,
+      breakLines: false,
+      autoStart: false,
+      loop: true,
+      deleteDelay: 2000,
+      loopDelay: 2000,
+    });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
